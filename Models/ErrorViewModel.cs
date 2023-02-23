cb308dc16d32a246bdd813a572d9e4b1c2a976cb
wanchaichab <wanchaichab@gmail.com>
2023-02-23 11:12:14 -0700

    test

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mission08_Group2_6.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
